8cd5ec070e7a204877a8b3b7f7d7cb9f2f18b837
Nassim Lahoudi <Nassim.Lahoudi@hotmail.com>
2026-03-26 00:09:11 +0100

    chore: add ESLint and Prettier configuration files

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'warn',
+  },
+}
